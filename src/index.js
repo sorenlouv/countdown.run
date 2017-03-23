baf1b4ff8eb3aa3a53682aa8ecd8142badfac425
Søren Louv-Jansen <slo@tradeshift.com>
2017-03-24 00:37:27 +0100

    LocalStorage support

--- src/index.js
+++ src/index.js
@@ -4,8 +4,17 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import mainReducer from './reducers';
 import App from './App';
+import {debounce} from 'lodash';
 
-const store = createStore(mainReducer);
+const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : {};
+
+const store = createStore(mainReducer, persistedState);
+
+const persistState = debounce(() => {
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+}, 100);
+
+store.subscribe(persistState);
 
 ReactDOM.render(
   <Provider store={store}>
