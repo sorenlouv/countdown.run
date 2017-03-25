121f44a1adc4740588e288a7b162bd11c2268830
Søren Louv-Jansen <sorenlouv@gmail.com>
2017-03-25 16:33:20 +0100

    Added Github link and credits

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import './App.css';
 class App extends Component {
   render () {
     return (
-      <div className="App">
-        <AddCountdown />
-        <CountdownList/>
+      <div className="app-container">
+        <div className="App">
+          <AddCountdown />
+          <CountdownList/>
+        </div>
+        <div className="footer">
+          Created by <a href="https://twitter.com/sqrendk">Søren Louv-Jansen</a>. Open-sourced on <a href="https://github.com/sqren/countdown.run">Github</a>
+        </div>
       </div>
     );
   }
