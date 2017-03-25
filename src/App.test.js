eb46d8ddff4f32e08599c18a85c0649fb6166244
Søren Louv-Jansen <sorenlouv@gmail.com>
2017-03-25 14:15:46 +0100

    Add Travis config

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import mainReducer from './reducers';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const store = createStore(mainReducer);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    div
+  );
 });
